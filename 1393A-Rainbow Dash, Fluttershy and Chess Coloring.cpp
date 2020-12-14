ec882606d2958a8a816410f3540b03e6e8fcff87
MD APU HOSEN <47341461+APU160139@users.noreply.github.com>
2020-12-14 21:09:04 +0600

    Add files via upload

---
+++ 1393A-Rainbow Dash, Fluttershy and Chess Coloring.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long int t,n,i,r;
+    scanf("%ld",&t);
+    for(i=1;i<=t;i++){
+        scanf("%ld",&n);
+        r=(n/2)+1;
+        printf("%ld\n",r);
+    }
+
+    return 0;
+}
