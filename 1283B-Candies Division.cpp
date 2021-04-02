43cc479e4cd5086681523ec78a7e06478f178922
MD APU HOSEN <47341461+APU160139@users.noreply.github.com>
2021-04-02 15:46:53 +0600

    Add files via upload

---
+++ 1283B-Candies Division.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int t,n,k,a;
+    cin>>t;
+    while(t--){
+        cin>>n>>k;
+        if(n%k==0)
+            cout<<n<<endl;
+        else{
+            a=n/k*k;
+            cout<<min(n,a+k/2)<<endl;
+        }
+    }
+    return 0;
+}
